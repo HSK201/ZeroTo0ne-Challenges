d4b29fe1cba84c81ebc39a78028717a40c0fa0ca
Developer 23 <alkharboush@hotmail.com>
2019-08-03 21:28:35 +0300

    Galaxy_Rainbow
    Show and Reset a rainbow colors by using HTML5 + CSS + JS

---
+++ galaxy.js
@@ -0,0 +1,37 @@
+//Implement the Button action
+
+/* button 1 */
+$('#action_1').click(function () {
+    $('#local_1').css('background-color', 'indigo');
+    $('#local_6').css('background-color', 'orange');
+});
+
+/* button 2 */
+$('#action_2').click(function () {
+    $('#local_2').css('background-color', 'blue');
+    $('#local_5').css('background-color','yellow');
+});
+
+/* button 3 */
+$('#action_3').click(function () {
+    $('#local_3').css('background-color','green');
+    $('#local_4').css('background-color','red');
+});
+
+/* button 4 */
+$('#action_4').click(function () {
+    $('#globalGalaxy').css('background-color','violet');
+    $('#globalGalaxy_1').css('background-color','violet'); 
+});
+
+/* button 5 */
+$('#action_5').click(function () {
+    $('#globalGalaxy').css('background-color','aquamarine');
+    $('#globalGalaxy_1').css('background-color','aquamarine');
+    $('#local_1').css('background-color','aliceblue');
+    $('#local_2').css('background-color','aliceblue'); 
+    $('#local_3').css('background-color','aliceblue');
+    $('#local_4').css('background-color','aliceblue'); 
+    $('#local_5').css('background-color','aliceblue');
+    $('#local_6').css('background-color','aliceblue'); 
+});
